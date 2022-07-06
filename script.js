3cb4bc162399bdcdbb397e902a583ef3fe12310e
sargishakobyann97 <sargishakobyann97.com>
2022-07-06 18:19:58 +0400

    commit2

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 let menu = document.querySelector('#menu')
 let menuList = document.querySelector('.menu-list')
 let close = document.querySelector('#close')
-
-
-menu.addEventListener('click', () =>
-    menuList.classList.toggle('show')
-)
-close.addEventListener('click', () =>
-    menuList.classList.toggle('show')
-)
